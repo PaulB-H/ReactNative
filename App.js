dcfab722d1db0eb852a73f6185e560dc9b2f3571
Paul Bernard-Hall <p.bernardhall@gmail.com>
2020-10-27 19:59:49 -0400

    created state in App.js and flatlist rendering items

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import Header from './components/Header';
+import 'react-native-get-random-values';
+import {v4 as uuid} from 'uuid';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'},
+  ]);
+
   return (
     <View style={styles.container}>
       <Header />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <Text>{item.text}</Text>}
+      />
     </View>
   );
 };
